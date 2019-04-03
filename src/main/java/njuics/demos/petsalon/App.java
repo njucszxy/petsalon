00907105b1e850aed9f4a640c7d80d4e9eee123e
Chun <caochun@gmail.com>
2019-04-03 20:33:06 +0800

    init

---
+++ src/main/java/njuics/demos/petsalon/App.java
@@ -0,0 +1,11 @@
+package njuics.demos.petsalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
